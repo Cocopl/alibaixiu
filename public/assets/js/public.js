2d7cef0ba6f483d2e267c9f1459f2e2476905fd3
Coco <1947606351@qq.com>
2019-08-30 11:34:56 +0800

    完成了轮播图、热门推荐、最新发布、随机推荐、用户评论的数据展示；热门推荐、随机推荐、最新评论为几个页面公有的

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -1,19 +1,48 @@
 // 向服务器端发送请求 索要文章分类列表数据
 $.ajax({
-    type: 'get',
-    url: '/categories',
-    success: function (response) {
-        var navTpl = `
-			{{each data}}
+    type: "get",
+    url: "/posts/random",
+    success: function(res) {
+        var tempT = `
+				{{each data}}
+				<li>
+				<a href="detail.html?id={{$value._id}}">
+					<p class="title">{{$value.title}}</p>
+					<p class="reading">阅读({{$value.views}})</p>
+					<div class="pic">
+						<img src="{{$value.thumbnail}}" alt="">
+					</div>
+				</a>
+			</li>
+				{{/each}}
+		`;
+        var html = template.render(tempT, { data: res });
+        $("#random").html(html);
+    }
+});
+
+$.ajax({
+    type: "get",
+    url: "/comments/lasted",
+    success: function(res) {
+        var commTpl = `
+			{{each date}}
 			<li>
-				<a href="list.html?categoryId={{$value._id}}">
-					<i class="fa {{$value.className}}"></i>{{$value.title}}
+			<a href="javascript:;">
+				<div class="avatar">
+					<img src="{{$value.author.avatar}}" alt="">
+				</div>
+				<div class="txt">
+					<p>
+						<span>{{$value.author.nickName}}</span>2020-02-01说:
+					</p>
+					<p>{{$value.content}}</p>
+					</div>
 				</a>
 			</li>
 			{{/each}}
 		`;
-        var html = template.render(navTpl, { data: response });
-        $('.navTemp').html(html)
-       
+        var htmlC = template.render(commTpl, { date: res });
+        $("#comment").html(htmlC);
     }
 })
